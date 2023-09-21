313955542f9a07a6e32f6a7af4564e9546332e4a
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-21 14:59:54 -0500

    feat: Created Paragraph special component and lib support file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import { useRef } from "react";
+import Paragraph from "@/ui/components/Paragraph";
 
 export default function Home() {
   return (
     <>
-      <main className="bg-red-500">Hello world</main>
+      <main className="bg-red-500">
+        <Paragraph size={"default"}>Some text</Paragraph>
+      </main>
     </>
   );
 }
