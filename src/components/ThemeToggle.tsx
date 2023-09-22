745ac0bb4a12f7772e5a05a8ac1907e8df6861b5
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-22 10:51:26 -0500

    fix: ThemeToggle was being perceived as a button child

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,26 +1,24 @@
 "use client";
 
+import * as React from "react";
 import { useTheme } from "next-themes";
-import { FC } from "react";
+
+import { Icons } from "@/components/Icons";
+import { Button } from "@/components/ui/Button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "./ui/DropdownMenu";
-import { Button } from "./ui/Button";
-import { Laptop, Moon, Sun } from "lucide-react";
-import Icons from "./Icons";
+} from "@/components/ui/DropdownMenu";
 
-interface ThemeToggleProps {}
-
-const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
+export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button variant={"ghost"} size={"sm"}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm">
           <Icons.Sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
           <Icons.Moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
           <span className="sr-only">Toggle theme</span>
@@ -42,6 +40,4 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-};
-
-export default ThemeToggle;
+}
